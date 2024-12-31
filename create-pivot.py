244f6a3ed96b8bb071684c18a64bd4d8928b5ce4
nicolacucina <nicola.cucina@studenti.unipg.it>
2024-12-31 14:14:10 +0100

    reorginize folders

--- create-pivot.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-ratings = pd.read_csv("ml-latest-small/ratings.csv")
-movies = pd.read_csv("ml-latest-small/movies.csv")
-
-user_movie_matrix = ratings.pivot(index="userId", columns="movieId", values="rating").fillna(0)
-print("User-movie matrix:")
-print(user_movie_matrix.head())
-pd.DataFrame(user_movie_matrix).to_csv("user_movie_matrix.csv")
